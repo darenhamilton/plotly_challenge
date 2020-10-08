4a0b8801afe7c14655d8ab9a10121a79923dda32
Hamilton <hamihomeme@gmail.com>
2020-10-08 11:22:36 -0700

    update code

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,8 @@ d3.json("samples.json").then((importedData) => {
     console.log(importedData);
   });
   
+  let data = importedData
+
+  let samples = Object.values(data.samples)
+  let metaData = Object.values(data.metadata)
+  let names = Object.values(data.names)
